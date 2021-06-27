93725e186627ed2e17e1a66391e2c64be10e06cc
Zeca-JC <jcarlosfera@gmail.com>
2021-06-27 13:54:26 -0300

    Correçao da fonte e outros erros

--- pages/form/form1.js
+++ pages/form/form1.js
@@ -22,6 +22,18 @@ inputs.forEach((input) => {
 const sec = document.querySelector('.container');
 const toggle = document.querySelector('.toggle');
 
+var tema = false;
+
 toggle.onclick = function(){
     sec.classList.toggle('dark');
+
+    if (tema == false) {
+        document.getElementById("claroEscuro").innerHTML = "Claro";  
+        document.getElementById("claroEscuro").style.color = "#fff";
+        tema = true;
+    } else {
+        document.getElementById("claroEscuro").innerHTML = "Escuro";    
+        document.getElementById("claroEscuro").style.color = "black";
+        tema = false;
+    }
 }
