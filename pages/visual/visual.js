3ef1086c44b5c1e42779078550d2b74833bd67de
Zeca-JC <jcarlosfera@gmail.com>
2021-06-27 13:32:41 -0300

    Correçao da fonte e outros erros

--- pages/visual/visual.js
+++ pages/visual/visual.js
@@ -4,8 +4,19 @@
 const sec = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
 
+var tema = false;
+
 toggle.onclick = function(){
     sec.classList.toggle('dark');
+
+
+    if (tema == false) {
+        document.getElementById("claroEscuro").innerHTML = "Claro";    
+        tema = true;
+    } else {
+        document.getElementById("claroEscuro").innerHTML = "Escuro";    
+        tema = false;
+    }
 }
 
 
